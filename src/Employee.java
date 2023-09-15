3975df6af423afa898bff592ff942071e130b532
serper35 <serper.wolf@gmail.com>
2023-09-16 01:35:37 +0300

    test.1

---
+++ src/Employee.java
@@ -0,0 +1,57 @@
+public class Employee {
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private int department;
+    private double salary;
+    private static int count = 0;
+    private int id;
+
+
+    public Employee (String firstName, String middleName, String lastName, int department, double salary) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.department = department;
+        this.salary = salary;
+        id = ++count;
+    }
+
+    public String getFirstName () {
+        return firstName;
+    }
+    public String getMiddleName () {
+        return middleName;
+    }
+    public String getLastName () {
+        return lastName;
+    }
+
+    public String getFullName () {
+        return firstName + " " + middleName+ " " + lastName;
+    }
+    public int getDepartment() {
+        return department;
+    }
+    public double getSalary () {
+        return salary;
+    }
+    public int getId () {
+        return id;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public void setDepartment (int department) {
+        this.department = department;
+    }
+    public void setSalary (double salary) {
+        this.salary = salary;
+    }
+    public String toString() {
+        return "\nId - " + id + "\nИмя - " + getFirstName() + "\nОтчество -  " + getMiddleName() + "\nФамилия - " + getLastName() + "\nДепартамент - " + getDepartment() + "\nЗарплата - " + getSalary();
+    }
+
+}
---
+++ src/EmployeeBook.java
@@ -0,0 +1,106 @@
+import java.util.Scanner;
+
+public class EmployeeBook {
+    private final Employee[] employees;
+    private int size ;
+
+    public EmployeeBook() {
+        this.employees = new Employee[10];
+    }
+
+    //Добавить сотрудника
+    public void addEmployee(String firstName, String middleName, String lastName, int department, double salary) {
+        Employee employee = new Employee(firstName, middleName, lastName, department, salary);
+        employees[size++] = employee;
+
+    }
+
+    //Получить список сотрудников.
+    public void allEmployee() {
+        for (int i = 0; i < size; i++) {
+            Employee all = employees[i];
+            System.out.println("Сотрудник №" + employees[i].getId() + "\n-----------------" + all.toString() + "\n");
+        }
+    }
+
+    public double totalSalary() {
+        double totalSalary = 0;
+        for (int i = 0; i < size; i++) {
+            Employee money = employees[i];
+            totalSalary += money.getSalary();
+        }
+        return totalSalary;
+    }
+
+    public void printTotalSalary() {
+        System.out.println("\nСумма затрат на зарплаты в месяц - " + totalSalary() + "\n-----------------");
+    }
+
+    public void minSalary() {
+        double minSalary = employees[0].getSalary();
+        Employee temp = employees[0];
+
+        for (int i = 0; i < size; i++) {
+            Employee salary = employees[i];
+            if (salary.getSalary() < minSalary) {
+                minSalary = salary.getSalary();
+                temp = salary;
+            }
+        }
+        System.out.println("\nМинимальная зарплата - " + minSalary + " у сотрудника " + temp.getFullName() + "\n-----------------");
+    }
+
+    public void maxSalary() {
+        double maxSalary = employees[0].getSalary();
+         Employee temp =employees[0];
+        
+        for (int i = 0; i < size; i++) {
+            Employee  salary = employees[i];
+            if (maxSalary < salary.getSalary()) {
+                maxSalary = salary.getSalary();
+                temp = salary;
+            }
+        }
+        System.out.println("\nМаксимальная зарплата - " + maxSalary + " у сотрудника " + temp.getFullName() + "\n-----------------");
+    }
+
+    public double averageSalary() {
+        return totalSalary() / size;
+    }
+
+    public void printAverageSalary() {
+        System.out.println("\nСреднее значение зарплат - " + averageSalary() + ".\n-----------------");
+    }
+
+    public void printFullNameEmployees() {
+        for (int i = 0; i < size; i++) {
+            System.out.println("\nФИО сотрудника №" + employees[i].getId() + " - " + employees[i].getFullName() + "." + "\n-----------------");
+        }
+    }
+
+    public void indexsalary() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите процент индексации зарплаты.");
+        double index = (scan.nextDouble()) /10;
+
+        for (int i = 0; i < size; i++) {
+            employees[i].setSalary((employees[i].getSalary() * index));
+        }
+    }
+
+    public void depMinSalary() {
+        Scanner scan = new Scanner(System.in);
+        int inputDepartament = scan.nextInt();
+        for (int i = 0; i < size; i++) {
+            double minSalary = 100000000000.0;
+            if (employees[i].getDepartment() == inputDepartament) {
+                if (employees[i].getSalary() < minSalary) {
+                    minSalary = employees[i].getSalary();
+
+                }
+            }
+
+        }
+    }
+
+}
